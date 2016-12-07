933d47200e0ee0aab48838206210e2fe74b28ef8
devatha k <k.n.devs@gmail.com>
2016-12-06 23:38:49 -0500

    changed structure

---
+++ extension/javascript/scripts.js
@@ -0,0 +1,33 @@
+console.log("added");
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+  document.getElementById("demo").innerHTML = "Hello World";
+
+
+
+    // chrome.tabs.create({'url': "/options.html" } )
+
+
+    // chrome.tabs.getSelected(null, function(tab) {
+    //   d = document;
+
+    //   var f = d.createElement('form');
+    //   // f.action = 'http://gtmetrix.com/analyze.html?bm';
+    //   f.action = 'http://gtmetrix.com/analyze.html?bm';
+    //   f.method = 'post';
+    //   var i = d.createElement('input');
+    //   i.type = 'hidden';
+    //   i.name = 'url';
+    //   i.value = tab.url;
+    //   f.appendChild(i);
+    //   d.body.appendChild(f);
+    //   f.submit();
+    // });
+  }, false);
+}, false);
+
--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
  
 module.exports = {
-  entry: './main.js',
+  entry: '../main.js',
   output: { path: __dirname, filename: 'bundle.js' },
   module: {
     loaders: [
@@ -11,7 +11,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015']
+          presets: ['es2015', 'react']
         }
       }
     ]
